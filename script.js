b9dbdaab8489570ca42cad4292a1b52853deac4e
Camila Costney <camila.costney@gmail.com>
2024-01-09 18:49:49 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+// let num3 = 15;
+// let num4 = 27;
+// console.log(num3 + num4);
+
+function conectando() {
+    console.log("Conexão feita com sucesso!");
+  }
+  
+  conectando();
+  conectando();
+  conectando();
